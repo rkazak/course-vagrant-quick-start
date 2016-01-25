82dfeb2898d2b527769083848f1cf2887305f801
Training <training@training>
2016-01-25 01:29:59 -0500

    Add Tomcat 8 provisioning

---
+++ vagrant-quickstart/provision-tomcat.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+apt-get update -y
+apt-get upgrade -y
+apt-get autoremove -y
+apt-get install -y nano git openjdk-7-jdk openjdk-7-jre-headless
+
+cd /usr/local
+wget http://ftp.wayne.edu/apache/tomcat/tomcat-8/v8.0.30/bin/apache-tomcat-8.0.30.tar.gz
+tar -xzvf apache-tomcat-8.0.30.tar.gz
+ln -s apache-tomcat-8.0.30 tomcat
+
+cp /vagrant/tomcat8 /etc/init.d/tomcat8
+chmod 755 /etc/init.d/tomcat8
+update-rc.d tomcat8 defaults
+service tomcat8 start
